d805ac2b3889e137f305210f9ad918cb5db93224
andre gois <andre.s.gois3@gmail.com>
2019-09-05 10:42:41 -0300

    v0.3 - Incluindo ações de expansão e contração nos botões

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,21 @@
 /* Botões de Controle */
-const expansion = document.getElementByid('expansion');
-const contraction = document.getElementByid('contraction');
+const expansion = document.getElementById('expansion');
+const contraction = document.getElementById('contraction');
 
 /* As duas abas: Menu e Conteúdo	*/
 const menu = document.querySelector('#menu');
 const conteudo = document.querySelector('#conteudo');
 
 expansion.addEventListener('click', function(){
-   
+   menu.classList.add('expansion');
+   conteudo.style.marginRight='-60%';
+   expansion.style.display='none';
+   contraction.style.display='block';
 });
 
+contraction.addEventListener('click', function(){
+   menu.classList.remove('expansion');
+   conteudo.style.marginRight='0%';
+   contraction.style.display='none';
+   expansion.style.display='block';
+});
