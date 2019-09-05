a098e6c53308df83c7139c3dcb400c23ff8d7e8c
andre gois <andre.s.gois3@gmail.com>
2019-09-05 09:24:49 -0300

    v0.2 - Inserindo botão - section conteudo

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+/* Botões de Controle */
+const expansion = document.getElementByid('expansion');
+const contraction = document.getElementByid('contraction');
+
+/* As duas abas: Menu e Conteúdo	*/
+const menu = document.querySelector('#menu');
+const conteudo = document.querySelector('#conteudo');
+
+expansion.addEventListener('click', function(){
+   
+});
+
